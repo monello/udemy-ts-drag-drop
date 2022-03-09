c72a781f787dc12e967e12c8eb433218404ce692
Morne Louw <louwmo01@stepstone.com>
2022-03-09 13:26:19 +0200

    Extract the form element from template and inject into target element

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
+    formElement: HTMLFormElement
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app') as HTMLDivElement;
+
+        // Grab the content from the HTML Template element (HTML Node) and pass in true to make a deep-copy)
+        // This creates a document-fragment
+        const importedNode = document.importNode(this.templateElement, true);
+
+        // Extract the HTML Element (the <form>) from the Fragment
+        this.formElement = importedNode.firstElementChild as HTMLFormElement;
+
+        // Now insert the form element
+        this.attach();
     }
 
+    private attach() {
+        // Insert the new element just after the opening tag of the targetted hostElement
+        this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
+    }
 }

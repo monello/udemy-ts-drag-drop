7d8f0c5b4408e97f145292bde2d1719d5c5e5b54
Morné Louw <louw.morne@gmail.com>
2022-03-13 13:38:34 +0200

    Added an autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
+// autobind decorator
+//  - The underscore variables tell TS that we don't need these input parameters,
+//      but we must add them to get to the parameter(s) we do need.
+const autobind = (
+    _: any, // target: any,
+    _2: string, // methodName: string,
+    descriptor: PropertyDescriptor
+) => {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        // Using a getter "get()", is an alternative way to add a "value" to the new PropertyDescriptor
+        // You cannot have a "value" and a "getter" (or "setter") on the same ProprtyDescriptor
+        // get() was picked here as it is a function gives us the correct context for the this-keyword (ie the
+        //    class/object the property that this ProperyDescriptor describes)
+        //    Its is also useful that is ie a function in case we wanted to add som extra logic before return the value.
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjustedDescriptor;
+
+}
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -32,7 +56,8 @@ class ProjectInput {
         this.attach();
     }
 
-    // The on-submit handler function for the forn
+    // The on-submit handler function for the form
+    @autobind
     private submitHandler(event: Event) {
         // Prevent the form from sending an HTTPRequest (it's default behaviour on-submit)
         event.preventDefault();
@@ -46,7 +71,7 @@ class ProjectInput {
         // Remember to bind the 'this' context to the submit-handler, else the submit handler's default 'this'
         //  context will be the HTML element it is attached to and not the class in which the event-handler function
         //  is defined (Just one of those lovely JS quirks)
-        this.formElement.addEventListener('submit', this.submitHandler.bind(this))
+        this.formElement.addEventListener('submit', this.submitHandler)
     }
 
     private attach() {

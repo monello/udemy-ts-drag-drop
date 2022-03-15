732c0e2254dccbd202b37a1ac261dc1c377570ee
Morné Louw <louw.morne@gmail.com>
2022-03-15 15:47:54 +0200

    Rendering Project Items with a Class

--- src/app.ts
+++ src/app.ts
@@ -167,6 +167,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 }
 
+// Project Item
+// -----------------------------------------------------------------------------------------------------------
+
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() { }
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
 // Project Lists
 // -----------------------------------------------------------------------------------------------------------
 
@@ -224,9 +246,7 @@ class ProjectList extends Component<HTMLElement, HTMLDivElement> {
         // now loop through all the currentprojects en add them to the listEl
         for (const prjItem of this.currentProjects) {
             // now build the new project list-item
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
         }
     }
 }

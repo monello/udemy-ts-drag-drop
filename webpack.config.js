e3b80942fac85654582231da34db0f9472cb8ee9
Morné Louw <louw.morne@gmail.com>
2022-03-16 14:05:16 +0200

    Finishing WebPack setup & add Dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 // A Node function to get and build an absolute path
-const path = require('path');
+const path = require('path')
 
 // This is a NodeJs export, as WebPack "speaks" Node
 module.exports = {
+    mode: 'development',
     entry: './src/app.ts',      // This tells webpack what the root-entry file of your project is
     output: {
         filename: 'bundle.js',  // What you want to call the final JS file (the file you'll import in your index.html)
-        path: path.resolve(__dirname, './dist')          // This is the target directory path where WP must save the file (must match your tsconfig "outdir")
+        path: path.resolve(__dirname, 'dist'),          // This is the target directory path where WP must save the file (must match your tsconfig "outdir")
+        publicPath: 'dist'
     },
-    // This tells WP that tsconfgig generates source-maps, which it should use to wire-up the mappings to the final bundled files
+    // This tells WebPack that tsconfgig generates source-maps, which it include, to use to wire-up the mappings to the final bundled files
     devtool: 'inline-source-map',
     // modules tells webpack how to process the TS files (a module is a file, when using ES Modules)
     module: {

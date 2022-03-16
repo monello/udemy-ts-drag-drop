ab03d7ca9ffe724acf67d4f74cf08fef67a2f96f
Morné Louw <louw.morne@gmail.com>
2022-03-16 12:11:03 +0200

    Adding TS support with ts-loader package

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,29 @@ const path = require('path');
 
 // This is a NodeJs export, as WebPack "speaks" Node
 module.exports = {
-    entry: './src/app.js',      // This tells webpack what the root-entry file of your project is
+    entry: './src/app.ts',      // This tells webpack what the root-entry file of your project is
     output: {
         filename: 'bundle.js',  // What you want to call the final JS file (the file you'll import in your index.html)
         path: path.resolve(__dirname, './dist')          // This is the target directory path where WP must save the file (must match your tsconfig "outdir")
+    },
+    // This tells WP that tsconfgig generates source-maps, which it should use to wire-up the mappings to the final bundled files
+    devtool: 'inline-source-map',
+    // modules tells webpack how to process the TS files (a module is a file, when using ES Modules)
+    module: {
+        // rules is where you list different kinds of files, we're going to list TS here, but can also add filetypes like CSS and more commonly SASS
+        rules: [
+            {
+                // a test webpack will perform on a file to see if it meets this ruke
+                test: /\.ts$/,
+                // which Wp-plugin to use. This ons is the package we installed along with WebPack (seen listed in the package.json). ts-loader
+                // ts-loader uses the existing tsconfig.json, so no extra configs are needed here
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        // which file types to process and bundle together
+        extensions: ['.ts', '.js']
     }
 };
